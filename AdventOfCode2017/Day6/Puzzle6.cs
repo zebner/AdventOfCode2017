b97b57c52b4c61b205952ef5fba833bc999497ee
Zach Ebner <zach.ebner@alkamitech.com>
2017-12-06 11:05:30 -0600

    Day 6

--- AdventOfCode2017/Day6/Puzzle6.cs
+++ AdventOfCode2017/Day6/Puzzle6.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2017.Puzzles
 {
@@ -7,12 +9,44 @@ namespace AdventOfCode2017.Puzzles
     {
         public static void Part1()
         {
-           throw new NotImplementedException("Puzzle 6.1 is not ready yet");
+            var banks = Array.ConvertAll(File.ReadAllText("Inputs/Puzzle6.txt").Split(), int.Parse);
+            var usedConfigurations = new List<int[]>();
+            Redistribute(usedConfigurations, banks);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Puzzle 6A answer is {usedConfigurations.Count}");
         }
 
         public static void Part2()
         {
-            throw new NotImplementedException("Puzzle 6.2 is not ready yet");
+            var banks = Array.ConvertAll(File.ReadAllText("Inputs/Puzzle6.txt").Split(), int.Parse);
+            var usedConfigurations = new List<int[]>();
+            Redistribute(usedConfigurations, banks);
+
+            var numInInfiniteLoop = usedConfigurations.Count - usedConfigurations.IndexOf(usedConfigurations.First(c => c.SequenceEqual(banks)));
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Puzzle 6A answer is {numInInfiniteLoop}");
+        }
+
+        private static void Redistribute(ICollection<int[]> usedConfigurations, IList<int> banks)
+        {
+            while (!usedConfigurations.Any(c => c.SequenceEqual(banks)))
+            {
+                usedConfigurations.Add(banks.ToArray());
+
+                var maxBlock = banks.ToList().IndexOf(banks.Max());
+                var blocksToDistribute = banks[maxBlock];
+                banks[maxBlock] = 0;
+                while (blocksToDistribute > 0)
+                {
+                    maxBlock++;
+                    if (maxBlock >= banks.Count)
+                        maxBlock = 0;
+
+                    banks[maxBlock]++;
+                    blocksToDistribute--;
+                }
+            }
         }
     }
 }
